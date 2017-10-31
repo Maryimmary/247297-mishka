adfcc91c8eca8f7358652fa23e30564a57c5b6ae
Maryimmary <maryimmary@yandex.ru>
2017-11-01 01:21:19 +0300

    fixed retina & svg

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,11 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -12,7 +17,10 @@ gulp.task("style", function() {
     .pipe(sass())
     .pipe(postcss([autoprefixer()]))
     .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(server.stream())
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"));
 });
 
 gulp.task("serve", ["style"], function () {
@@ -26,4 +34,29 @@ gulp.task("serve", ["style"], function () {
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("images", function () {
+  return gulp.src("img/**/*.{png,jpg,svg,webp}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("img"));
 });
